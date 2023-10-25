920e39b709e2c9ee1f2b2858a0ab72879805b002
marquis brunbdage <marquisbrunbdage@marquiss-MacBook-Pro.local>
2023-10-25 11:01:00 -0400

    created peseudo code for functions and unit test

---
+++ test.js
@@ -0,0 +1,32 @@
+/* 
+     unit Test 
+    Expect multiplication(4, 8) to be a number 
+    Expect Multiplication( 4,8) to equal to 32 
+    Expect multiplication ('f', 8) to be an error
+    Expect multiplication ('u', 't') to be an error
+    Expect concatOdds  ([1,6,,8],[3,17,24,56,11,31]) should result in ([1,3,11,17,31])
+    Expect concat Odds ([1,6,,1],[3,3,15,56,11,32]) should result to ([1,3,15,11])
+    Expect Concat Odds ([2,6,8], [12,12,42,32,],[10,14,16,] should result in  "Odds not found"
+
+   Functionality 
+   When a user "clicks" on calculate when information of their weight,height, wait circumference or selected gender, and erorr prompted"Please fill outMale or Female , Please fill out weight, Please fill out height, Please fill out circumference"
+   When the user "clicks"calculate  with gender selected but no information filled out for weight,height, and circumference, an error will be prompted " Please fill out weight, Please fill out height, Please fill out circumference"
+   When the user "clicks" calculate  with gender selected ,weight filled out, but not for height and circumference, an error will be prompted  " Please fill out height, Please fill out circumference"
+   When the user "clicks" calculate  with gender selected ,weight, and height filled out, but not circumference, an error will be prompted " Please fill out height, Please fill out circumference"
+   When the user "clicks" calculate the information will be processed and the user will be a shown a their results and a chart to where they are at with their fat percentage and weight. 
+     
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
